e3ce796293863410bbc225668b5107e082b9bca8
Zachariah Brown <crazyzprogrammer@hotmail.com>
2014-01-23 00:54:18 -0500

    Updated settings to provide more control

--- settings/settings.php
+++ settings/settings.php
@@ -1,16 +1,23 @@
 <?php
-/* Priority is the order of declaration */
+/* State of the Jam - only 1 may be active at once */
+$NextJamQued = true; // Generally Used with theme selection
+$ThemeVisible = false; // Used when voting is finished before the jam is running
+$JamRunning = false; // Used with submitting and editing game entries
+$JamCompleted = false; // Used with editing game entries before the next jam
+
+/* Any of the following may be active at any time - note above and make smart choices */
+$ThemeSuggestionsOpen = true;
+
+$ThemeVotingOpen = false;
+
+$AddGamesActive = false;
+
+$EditGamesActive = false;
+
+$JamGalleryActive = false;
+
 /* $ActiveJam must be set to a valid jam ID in the database */
-/* Only one value should be active at once */
-$ThemeSuggestionsActive = true;
-$ThemeVotingActive = false;
-$JamActive = false;
-$NextJamActive = false;
 $ActiveJamID = 1;
 $MaxSuggestions = 2;
 $MaxVotes = 1;
-
-/* Can be combined with the options above */
-$JamGallaryActive = false;
-$JamGalleryID = -1;
 ?>
