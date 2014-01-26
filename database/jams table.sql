3b306b3b4217859db1e77578d463864b79ae894d
Zachariah Brown <crazyzprogrammer@hotmail.com>
2014-01-26 15:11:35 -0500

    Cleaned up the sql scripts

--- database/jams table.sql
+++ database/jams table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `jams` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Title` text NOT NULL,
-  `BeginTime` text NOT NULL,
-  `EndTime` text NOT NULL,
-  `ChosenTheme` text NOT NULL,
-  `GalleryOpen` tinyint(1) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`ID`)
+CREATE TABLE jams (
+  ID int(11) NOT NULL AUTO_INCREMENT,
+  Title text NOT NULL,
+  BeginTime text NOT NULL,
+  EndTime text NOT NULL,
+  ChosenTheme text NOT NULL,
+  GalleryOpen tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (ID)
 )
--- database/users table.sql
+++ database/users table.sql
@@ -1,8 +1,10 @@
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Username` text NOT NULL,
-  `Password` text NOT NULL,
-  `Salt` text NOT NULL,
-  `IsAdmin` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`ID`)
+CREATE TABLE users (
+  ID int(11) NOT NULL AUTO_INCREMENT,
+  Username text NOT NULL,
+  Password text NOT NULL,
+  Salt text NOT NULL,
+  LastIP text NOT NULL,
+  IsBanned tinyint(1) NOT NULL DEFAULT 0,
+  IsAdmin tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (ID)
 )
