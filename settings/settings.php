a77a9e2d2f37d4e1115ee858af6765f85e0dd32b
Zachariah Brown <crazyzprogrammer@hotmail.com>
2014-01-27 10:03:47 -0500

    Updated the settings with a few more control options

--- settings/settings.php
+++ settings/settings.php
@@ -1,6 +1,7 @@
 <?php
 /* State of the Jam - only 1 may be active at once */
 $NextJamQued = false; // Generally Used with theme selection
+$WaitingOnTheme = false; // Used when voting is finished and before the jam is visible
 $ThemeVisible = false; // Used when voting is finished before the jam is running
 $JamRunning = false; // Used with submitting and editing game entries
 $JamCompleted = false; // Used with editing game entries before the next jam
@@ -14,10 +15,12 @@ $AddGamesActive = false;
 
 $EditGamesActive = false;
 
+$ThemeResultsVisible = false;
+
 $JamGalleryActive = false;
 
 /* $ActiveJam must be set to a valid jam ID in the database */
 $ActiveJamID = -1;
 $MaxSuggestions = 2;
-$MaxVotes = 1;
+$MaxVotes = 4;
 ?>
